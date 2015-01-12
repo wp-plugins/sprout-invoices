318d7053b6ce04c6df7c443afe61ca3528711405
dancameron <dancameron@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-01-12 19:24:13 +0000

    Deploy version 3.2
    git-svn-id: https://plugins.svn.wordpress.org/sprout-invoices/tags/3.2@1066302 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ views/notifications/estimate.php
@@ -0,0 +1,23 @@
+[name]
+
+[admin_note]
+
+---------------------------------------------
+Estimate Summary
+---------------------------------------------
+
+[estimate_subject]
+
+Estimate ID: [estimate_id]
+Estimate date: [estimate_issue_date]
+Client: [client_name]
+P.O. Number: [estimate_po_number]
+Amount: [estimate_total]
+
+[line_item_plain_list]
+
+You can view the estimate here:
+[estimate_url]
+
+Thank you!
+---------------------------------------------
