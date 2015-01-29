74da4b73a53ccc2ecaac1a86536fec1e7ebbc925
dancameron <dancameron@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-01-29 16:42:31 +0000

    Deploy version 4.1.2
    git-svn-id: https://plugins.svn.wordpress.org/sprout-invoices/tags/4.1.2@1078354 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ views/admin/meta-boxes/estimates/send.php
@@ -0,0 +1,10 @@
+<div id="send_doc_options_wrap" class="admin_fields clearfix">
+	<?php sa_admin_fields( $fields, 'send_metabox' ); ?>
+	<div id="quick_send_option" class="form-group">
+		<span class="label_wrap">&nbsp;</span>
+		<div class="input_wrap">
+			<a href="javascript:void(0)" class="button" id="send_doc_notification"><?php si_e('Send Estimate') ?></a> <span class="helptip" title="<?php si_e("Make sure to save all information before sending your estimate.") ?>"></span>
+		</div>
+	</div>
+	
+</div>
