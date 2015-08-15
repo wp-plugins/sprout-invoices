5f9d1cf1443e42edb86340c0fa936488a5516509
dancameron <dancameron@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-08-15 01:09:59 +0000

    Deploy version 8.0.5
    git-svn-id: https://plugins.svn.wordpress.org/sprout-invoices/tags/8.0.5@1221349 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ controllers/estimates/Estimates.php
@@ -0,0 +1,158 @@
+<?php
+
+/**
+ * Estimates Controller
+ *
+ *
+ * @package Sprout_Invoice
+ * @subpackage Estimates
+ */
+class SI_Estimates extends SI_Controller {
+	const HISTORY_UPDATE = 'si_history_update';
+	const HISTORY_STATUS_UPDATE = 'si_history_status_update';
+	const HISTORY_INVOICE_CREATED = 'si_invoice_created';
+	const VIEWED_STATUS_UPDATE = 'si_viewed_status_update';
+
+	public static function init() {
+
+		// Unique urls
+		add_filter( 'wp_unique_post_slug', array( __CLASS__, 'post_slug'), 10, 4 );
+
+		// Adjust estimate id and status after clone
+		add_action( 'si_cloned_post',  array( __CLASS__, 'adjust_cloned_estimate' ), 10, 3 );
+
+		// Notifications
+		add_filter( 'wp_ajax_sa_send_est_notification', array( __CLASS__, 'maybe_send_notification' ) );
+
+	}
+
+	///////////////
+	// Re-writes //
+	///////////////
+
+	/**
+	 * Filter the unique post slug.
+	 *
+	 * @param string $slug          The post slug.
+	 * @param int    $post_ID       Post ID.
+	 * @param string $post_status   The post status.
+	 * @param string $post_type     Post type.
+	 * @param int    $post_parent   Post parent ID
+	 * @param string $original_slug The original post slug.
+	 */
+	public static function post_slug( $slug, $post_ID, $post_status, $post_type ) {
+		if ( $post_type == SI_Estimate::POST_TYPE ) {
+			return $post_ID;
+		}
+		return $slug;
+	}
+
+
+	/////////////////////
+	// AJAX Callbacks //
+	/////////////////////
+
+	public static function maybe_send_notification() {
+		// form maybe be serialized
+		if ( isset( $_REQUEST['serialized_fields'] ) ) {
+			foreach ( $_REQUEST['serialized_fields'] as $key => $data ) {
+				if ( strpos( $data['name'], '[]' ) !== false ) {
+					$_REQUEST[str_replace( '[]', '', $data['name'] )][] = $data['value'];
+				}
+				else {
+					$_REQUEST[$data['name']] = $data['value'];
+				}
+			}
+		}
+		if ( ! isset( $_REQUEST['sa_send_metabox_notification_nonce'] ) ) {
+			self::ajax_fail( 'Forget something (nonce)?' ); }
+
+		$nonce = $_REQUEST['sa_send_metabox_notification_nonce'];
+		if ( ! wp_verify_nonce( $nonce, SI_Controller::NONCE ) ) {
+			self::ajax_fail( 'Not going to fall for it!' ); }
+
+		if ( ! isset( $_REQUEST['sa_send_metabox_doc_id'] ) ) {
+			self::ajax_fail( 'Forget something (id)?' ); }
+
+		if ( get_post_type( $_REQUEST['sa_send_metabox_doc_id'] ) !== SI_Estimate::POST_TYPE ) {
+			return;
+		}
+
+		$recipients = ( isset( $_REQUEST['sa_metabox_recipients'] ) ) ? $_REQUEST['sa_metabox_recipients'] : array();
+
+		if ( isset( $_REQUEST['sa_metabox_custom_recipient'] ) && '' !== trim( $_REQUEST['sa_metabox_custom_recipient'] ) ) {
+			if ( is_email( $_REQUEST['sa_metabox_custom_recipient'] ) ) {
+				$recipients[] = $_REQUEST['sa_metabox_custom_recipient'];
+			}
+		}
+
+		if ( empty( $recipients ) ) {
+			self::ajax_fail( 'A recipient is required.' );
+		}
+
+		$estimate = SI_Estimate::get_instance( $_REQUEST['sa_send_metabox_doc_id'] );
+		$estimate->set_sender_note( $_REQUEST['sa_send_metabox_sender_note'] );
+
+		$from_email = null;
+		$from_name = null;
+		if ( isset( $_REQUEST['sa_send_metabox_send_as'] ) ) {
+			$name_and_email = SI_Notifications_Control::email_split( $_REQUEST['sa_send_metabox_send_as'] );
+			if ( is_email( $name_and_email['email'] ) ) {
+				$from_name = $name_and_email['name'];
+				$from_email = $name_and_email['email'];
+			}
+		}
+
+		do_action( 'send_estimate', $estimate, $recipients, $from_email, $from_name );
+
+		// If status is temp than change to pending.
+		if ( $estimate->get_status() !== SI_Estimate::STATUS_APPROVED ) {
+			$estimate->set_pending();
+		}
+
+		header( 'Content-type: application/json' );
+		if ( self::DEBUG ) { header( 'Access-Control-Allow-Origin: *' ); }
+		echo wp_json_encode( array( 'response' => si__( 'Notification Queued' ) ) );
+		exit();
+	}
+
+	////////////
+	// Misc. //
+	////////////
+
+	/**
+	 * Adjust the estimate id
+	 * @param  integer $new_post_id
+	 * @param  integer $cloned_post_id
+	 * @param  string  $new_post_type
+	 * @return
+	 */
+	public static function adjust_cloned_estimate( $new_post_id = 0, $cloned_post_id = 0, $new_post_type = '' ) {
+		if ( get_post_type( $new_post_id ) == SI_Estimate::POST_TYPE ) {
+			$og_estimate = SI_Estimate::get_instance( $cloned_post_id );
+			$og_id = $og_estimate->get_estimate_id();
+			$estimate = SI_Estimate::get_instance( $new_post_id );
+
+			// Adjust estimate id
+			$new_id = apply_filters( 'si_adjust_cloned_estimate_id', $og_id . '-' . $new_post_id, $new_post_id, $cloned_post_id );
+			$estimate->set_estimate_id( $new_id );
+
+			// Adjust status
+			$estimate->set_pending();
+		}
+	}
+
+	/////////////
+	// utility //
+	/////////////
+
+	public static function is_edit_screen() {
+		$screen = get_current_screen();
+		$screen_post_type = str_replace( 'edit-', '', $screen->id );
+		if ( $screen_post_type == SI_Estimate::POST_TYPE ) {
+			return true;
+		}
+		return false;
+	}
+
+}
---
+++ resources/admin/js/sprout_invoice.js
@@ -0,0 +1,285 @@
+jQuery.noConflict();
+
+function si_format_money ( value ) {
+
+	if ( typeof Intl !== 'object' ) {
+		var parts = value.toString().split(si_js_object.localeconv.mon_decimal_point);
+		parts[0] = parts[0].replace( /\B(?=(\d{3})+(?!\d))/g, si_js_object.localeconv.mon_thousands_sep );
+		return si_js_object.localeconv.currency_symbol + parts.join(si_js_object.localeconv.mon_decimal_point);
+	}
+	var cformatter = new Intl.NumberFormat( si_js_object.locale_standard, {
+		style: 'currency',
+		currency: si_js_object.localeconv.int_curr_symbol.trim(),
+		maximumFractionDigits: si_js_object.localeconv.int_frac_digits,
+		minimumFractionDigits: si_js_object.localeconv.int_frac_digits,
+	});
+	return cformatter.format( value );
+}
+
+jQuery(function($) {
+
+	/**
+	 * select2 init
+	 */
+	$('.select2').select2({
+		// Support for optgroup searching
+		matcher: function modelMatcher (params, data) {
+			data.parentText = data.parentText || "";
+
+			// Always return the object if there is nothing to compare
+			if ($.trim(params.term) === '') {
+				return data;
+			}
+
+			// Do a recursive check for options with children
+			if (data.children && data.children.length > 0) {
+				// Clone the data object if there are children
+				// This is required as we modify the object to remove any non-matches
+				var match = $.extend(true, {}, data);
+
+				// Check each child of the option
+				for (var c = data.children.length - 1; c >= 0; c--) {
+					var child = data.children[c];
+					child.parentText += data.parentText + " " + data.text;
+
+					var matches = modelMatcher(params, child);
+
+					// If there wasn't a match, remove the object in the array
+					if (matches == null) {
+						match.children.splice(c, 1);
+					}
+				}
+
+				// If any children matched, return the new object
+				if (match.children.length > 0) {
+					return match;
+				}
+
+				// If there were no matching children, check just the plain object
+				return modelMatcher(params, match);
+			}
+
+			// If the typed-in term matches the text of this term, or the text from any
+			// parent term, then it's a match.
+			var original = (data.parentText + ' ' + data.text).toUpperCase();
+			var term = params.term.toUpperCase();
+
+
+			// Check if the text contains the term
+			if (original.indexOf(term) > -1) {
+				return data;
+			}
+
+			// If it doesn't contain the term, don't return anything
+			return null;
+		}
+	});
+
+	/**
+	 * Tooltip with qtip
+	 * @type {}
+	 */
+	$('.si_tooltip[title!=""], .helptip[title!=""]').qtip({
+		style: {
+			classes: 'qtip-bootstrap'
+		}
+	});
+
+    jQuery("#the-list .doc_status_change").live('click', function(e) {
+		e.preventDefault();
+		var $status_change_link = $( this ),
+			$status_button = $( this ).closest('.quick_status_update'),
+			$row_actions = $status_change_link.closest( '.row-actions' ),
+			$new_status = $status_change_link.data( 'status-change' ),
+			$id = $status_change_link.data( 'id' ),
+			$nonce = $status_change_link.data( 'nonce' ),
+			$status_span = $( '#status_' + $id );
+
+		$status_button.html(si_js_object.inline_spinner);
+		
+		$.post( ajaxurl, { action: 'si_change_doc_status', id: $id, status: $new_status, change_status_nonce: $nonce },
+			function( data ) {
+				if ( data.error ) {
+					$status_span.html( data.response );	
+				}
+				else {
+					$button_html = $( data.new_button ).html();
+					// swap out the button with the new one
+					$status_button.html( $button_html );
+				};
+				return data;
+			}
+		);
+	});
+
+
+	/**
+	 * License Activation
+	 */
+	$('#free_license').on('click', function(e) {
+		e.stopPropagation();
+		e.preventDefault();
+		var $button = $( this ),
+			$license_key = $('#si_license_key').val(),
+			$license_message = $('#license_message');
+
+		$button.hide();
+		$button.after(si_js_object.inline_spinner);
+		$.post( ajaxurl, { action: 'si_get_license', license: $license_key, security: si_js_object.security },
+			function( data ) {
+				$('.spinner').hide();
+				if ( data.error ) {
+					$button.show();
+					$license_message.html('<span class="inline_error_message">' + data.response + '</span>');
+				}
+				else {
+					$('#si_license_key').val(data.license);
+					$license_message.html('<span class="inline_success_message">' + data.response + '</span>');
+				}
+			}
+		);
+	});
+
+	/**
+	 * License Activation
+	 */
+	$('#activate_license').on('click', function(e) {
+		e.stopPropagation();
+		e.preventDefault();
+		var $button = $( this ),
+			$license_key = $('#si_license_key').val(),
+			$license_message = $('#license_message');
+
+		$button.hide();
+		$button.after(si_js_object.inline_spinner);
+		$.post( ajaxurl, { action: 'si_activate_license', license: $license_key, security: si_js_object.security },
+			function( data ) {
+				$('.spinner').hide();
+				if ( data.error ) {
+					$button.show();
+					$license_message.html('<span class="inline_error_message">' + data.response + '</span>');	
+				}
+				else {
+					$license_message.html('<span class="inline_success_message">' + data.response + '</span>');
+				}
+			}
+		);
+	});
+
+	/**
+	 * License Deactivation
+	 */
+	$('#deactivate_license').on('click', function(e) {
+		e.stopPropagation();
+		e.preventDefault();
+		var $button = $( this ),
+			$activate_button = $('#activate_license');
+			$license_key = $('#si_license_key').val(),
+			$license_message = $('#license_message');
+
+		$button.hide();
+		$button.after(si_js_object.inline_spinner);
+		$.post( ajaxurl, { action: 'si_deactivate_license', license: $license_key, security: si_js_object.security },
+			function( data ) {
+				$('.spinner').hide();
+				if ( data.error ) {
+					$button.show();
+					$license_message.html('<span class="inline_error_message">' + data.response + '</span>');	
+				}
+				else {
+					$activate_button.hide();
+					$activate_button.removeAttr('disabled').addClass('button-primary').fadeIn();
+					$license_message.html('<span class="inline_success_message">' + data.response + '</span>');
+				}
+			}
+		);
+	});
+
+
+});
+
+;(function( $, si, undefined ) {
+
+	si.siAdmin = {
+		config: {
+			failed_save: false
+		},
+	};
+
+	si.siAdmin.InitAjaxSettings = function() {
+
+		$(".ajax_save").change( function( e ) {
+
+			// If the form is failing don't attempt again.
+			if ( si.siAdmin.config.failed_save ) { return };
+
+			// handle the payments form differently, only post if the payment selector is chosen.
+			if ( $(this).hasClass('group-buying/payment') ) {
+				if ( e.target.id == 'si_payment_processor' ) {
+					si.siAdmin.ajax_post_form( $(this) );
+					return;
+				};
+			}
+			// handle the full page ajax pages differently 
+			else if ( $(this).hasClass( 'full_page_ajax' ) ) {
+				si.siAdmin.ajax_post_form( $(this) );
+				return;
+			}
+			else {
+				si.siAdmin.ajax_post_options( $(this) );
+				return;
+			};
+		});
+	};
+
+
+
+	// Use wp_ajax to update each option without a full page return.
+	si.siAdmin.ajax_post_options = function( form ) {
+		var $form_dialog = $("#ajax_saving");
+		si.siAdmin.show_dialog();
+		$.post( ajaxurl, { action: 'si_save_options', options: form.serialize() },
+			function( data ) {
+				$form_dialog.html(data).fadeOut();
+			}
+		);
+	};
+
+	// submit the form in the background and replace the DOM
+	si.siAdmin.ajax_post_form = function( form ) {
+		var $form_dialog = $("#ajax_saving");
+		si.siAdmin.show_dialog();
+		$.ajax( {
+			type: "POST",
+			url: form.attr( 'action' ),
+			data: form.serialize(),
+			success: function( response ) {
+				var new_form = $('<div />').html(response).find('form.ajax_save').html();
+				if ( new_form.length > 0 ) {
+					$form_dialog.html('Saved').fadeOut();
+					form.html(new_form);
+				}
+				else {
+					$form_dialog.html('Auto save failed, use "Save Changes" button.').fadeOut();
+					si.siAdmin.config.failed_save = true;
+				};
+			}
+		});
+	};
+
+	si.siAdmin.show_dialog = function( $html ) {
+		var $form_dialog = $("#ajax_saving");
+		$form_dialog.html( $('#ajax_saving').data( 'message' ) );
+		$form_dialog
+			.css('position', 'fixed')
+			.css('left', '45%')
+			.css('top', '45%')
+			.show();
+	};
+
+})( jQuery, window.si = window.si || {} );
+
+// Init
+jQuery(function() {
+	si.siAdmin.InitAjaxSettings();
+});
